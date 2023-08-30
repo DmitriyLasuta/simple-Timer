e946e5a74b083e693ed4262f5e3761d1f3ca7ba7
DmitriyLasuta <lasutadima01@gmail.com>
2023-08-30 04:06:50 +0300

    feat: add functions startTimer setTimerValues clearTimer

---
+++ app.js
@@ -0,0 +1,46 @@
+const timer = document.getElementById('timer-form')
+
+const countdownNumbers = {
+	tensMinutes: document.getElementById('tens-minutes'),
+	minutes: document.getElementById('minutes'),
+	tensSeconds: document.getElementById('tens-seconds'),
+	seconds: document.getElementById('seconds'),
+}
+
+let interval = null
+
+const startTimer = time => {
+	const endTime = Date.now() + time * 60000
+	interval = setInterval(() => {
+		const currentTime = Date.now()
+		const timeRemaining = endTime - currentTime
+		if (timeRemaining > 0) {
+			setTimerValues({
+				tensMinutes: ~~(timeRemaining / 1000 / 60 / 10),
+				minutes: ~~((timeRemaining / 1000 / 60) % 10),
+				tensSeconds: ~~(((timeRemaining / 1000) % 60) / 10),
+				seconds: ~~(((timeRemaining / 1000) % 60) % 10),
+			})
+		} else {
+			clearInterval(interval)
+			return
+		}
+	}, 500)
+}
+
+const setTimerValues = ({ tensMinutes, minutes, tensSeconds, seconds }) => {
+	countdownNumbers.tensMinutes.textContent = tensMinutes
+	countdownNumbers.minutes.textContent = minutes
+	countdownNumbers.seconds.textContent = seconds
+	countdownNumbers.tensSeconds.textContent = tensSeconds
+}
+
+const clearTimer = () => {
+	if (interval) clearInterval(interval)
+	setTimerValues({
+		tensMinutes: '0',
+		minutes: '0',
+		tensSeconds: '0',
+		seconds: '0',
+	})
+}
